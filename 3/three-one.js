d6bc85437cfc9edcec1724a261662c65a45fc007
b19karhj <b19karhj@student.his.se>
2021-12-03 11:13:53 +0100

    Completed

---
+++ 3/three-one.js
@@ -0,0 +1,48 @@
+const fs = require('fs');
+var zero = 0;
+var one = 0;
+char = 0;
+
+const textToArray = (path) => {
+    const text = fs.readFileSync(path, 'utf8');
+    const textByLine = text.split('\n');
+    return textByLine
+}
+
+var array = textToArray('day3.txt')
+var gammaRateArray = [];
+var epsilonRateArray = [];
+while(char < 12){
+    for(i = 0; i < array.length; i++){ 
+        if(array[i].charAt(char) == 0){
+            zero += 1
+        
+        }
+        else if(array[i].charAt(char) == 1){
+            one += 1
+        }
+    }
+    char++;  
+    if(zero > one){
+        gammaRateArray.push (0)
+        epsilonRateArray.push (1)
+    }
+    else{
+        gammaRateArray.push (1)
+        epsilonRateArray.push (0)
+    }
+    var zero = 0;
+    var one = 0;
+}
+ 
+let gRate = gammaRateArray.join('')
+gRate = parseInt(gRate,2)
+let eRate = epsilonRateArray.join('')
+eRate =parseInt(eRate,2)
+
+console.log(gRate*eRate)
+
+
+
+
+
